a4a4115e97efb6526c534ba2eb280ff67ebbcc10
vinnietito <vincentnjuguna69@gmail.com>
2025-06-06 00:59:36 +0300

    Commits

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,12 +1,13 @@
-const mysql = require('mysql2');
-require('dotenv').config();
+const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  connectionLimit: 10
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-module.exports = pool.promise();
+module.exports = pool;
